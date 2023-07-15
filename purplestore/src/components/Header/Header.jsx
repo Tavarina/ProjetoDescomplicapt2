ce8e1967ef86cfba39b82a6eb9dccc556b955c06
Tavarina <fabi.ana_deoliveira@outlook.com>
2023-07-15 00:12:58 -0300

    iniciando projeto loja

--- purplestore/src/components/Header/Header.jsx
+++ purplestore/src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import SearchBar from '../SearchBar/SearchBar';
+import './Header.css';
+import CartButton from '../CartButton/CartButton';
 
 function Header() {
   return (
-    <header className="header" style={{ background: 'red' }}>
+    <header className="header">
       <div className="container">
         <SearchBar />
+        <CartButton />
       </div>
     </header>
   );
--- purplestore/src/components/SearchBar/SearchBar.jsx
+++ purplestore/src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { BsSearch } from 'react-icons/bs';
+
+import './SearchBar.css';
+import fetchProducts from '../../api/fetchProducts';
+import AppContext from '../../context/AppContext';
 
 function SearchBar() {
+
+  const { setProducts, setLoading } = useContext(AppContext);
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    const products = await fetchProducts(searchValue);
+
+    setProducts(products);
+    setLoading(false);
+    setSearchValue('');
+  };
+
   return (
-    <form>
+    <form className="search-bar" onSubmit={handleSearch}>
+      {name}
       <input
         type="search"
+        value={searchValue}
         placeholder="Buscar produtos"
         className="search__input"
+        onChange={ ({ target }) => setSearchValue(target.value) }
         required
       />
-      <button
-        type="submit"
-        className="search__button">
-        icone
+
+      <button type="submit" className="search__button">
+        <BsSearch />
       </button>
     </form>
   );
